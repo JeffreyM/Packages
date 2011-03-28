142d245ab7d50d23eb9e95cd3f0234e271780fb4
Jeffrey McDonnell <jeffrey.mcdonnell@gmail.com>
2011-03-28 15:05:16 -0700

    More install updates

--- install-node.sh
+++ install-node.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-echo Installing node v$NODE_VERSION
-
 # git clone git://github.com/JeffreyM/Packages.git
 # cd ./Packages
 NODE_VERSION=$(cat ./latest)
+echo Installing node v$NODE_VERSION
 sudo mv ./node-v$NODE_VERSION-$HOSTTYPE /usr/local/bin/node
 sudo chmod 0755 /usr/local/bin/node
 
